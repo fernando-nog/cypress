6377ff4e0dd5704089bf1eaa693a48cac7ae13c5
fernando-nogueira <idd_fnogueira@uolinc.com>
2020-07-13 14:11:37 -0300

    add promise spec

--- cypress/integration/promises.spec.js
+++ cypress/integration/promises.spec.js
@@ -5,19 +5,16 @@ it('sem testes, ainda', () => {
 
 const getSomething = () => {
     return new Promise((resolve, reject) => {
-        callback => {
-            setTimeout(() => {
-                resolve(12);
-            }, 1000);
-        };
+        setTimeout(() => {
+            resolve(13);
+        }, 1000);
     });
 }
 
-const system = () => {
+const system = async () => {
     console.log('init');
-    getSomething().then(some => {
-        console.log(`Something is ${some}`);
-    });
+    const some = await getSomething().then();
+    console.log(`Something is ${some}`);
     console.log('end');
 }
 
