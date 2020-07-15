7c45790eb21de7bfc9f97d4cc2ffaf6829ebfefe
fernando-nogueira <idd_fnogueira@uolinc.com>
2020-07-15 14:40:07 -0300

    Add describe spec

---
+++ cypress/integration/describe.spec.js
@@ -0,0 +1,15 @@
+/// <reference types="cypress" />
+
+it('A external test', () => {
+
+});
+
+describe('Shoud group tests', () => {
+
+    describe('Shoud group inside group', () => {
+        it('Inside test', () => {
+
+        });
+    });
+
+});
