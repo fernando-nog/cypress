52e5c9a9725f7938d206d9ded1db3f139fcdc6a1
fernando-nogueira <idd_fnogueira@uolinc.com>
2020-07-16 18:01:13 -0300

    Add asserts

---
+++ cypress/integration/asserts.spec.js
@@ -0,0 +1,43 @@
+/// <reference types="cypress" />
+
+it('Equality', () => {
+    const a = 1;
+
+    expect(a).equal(1);
+    expect(a, 'Deveria ser 1').equal(1);
+    expect(a).to.be.equal(1);
+    expect(a).not.to.be.equal('b');
+});
+
+it('Truthy', () => {
+    const a = true;
+    const b = null;
+    let c;
+
+    expect(a).to.be.true;
+    expect(true).to.be.true;
+    expect(b).to.be.null;
+    expect(a).to.be.not.null;
+    expect(c).to.be.undefined;
+});
+
+it('Object Equality', () => {
+
+    const obj = {
+        a: 1,
+        b: 2
+    };
+
+    expect(obj).equal(obj);
+    expect(obj).equals(obj);
+    expect(obj).eq(obj);
+    expect(obj).to.be.equal(obj);
+    expect(obj).to.be.deep.equal({a: 1, b:2 });
+    expect(obj).eql({a: 1, b:2 });
+    expect(obj).include({a: 1});
+    expect(obj).to.have.property('b');
+    expect(obj).to.have.property('b', 2);
+    expect(obj).to.not.be.empty;
+    expect({}).to.be.empty;
+
+});
